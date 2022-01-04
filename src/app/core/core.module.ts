53028e62e1aa36908e2349d8216bdc356fe38659
Philip Hell <blvckbytes@gmail.com>
2022-01-04 16:05:13 +0100

    Worked on datepick

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TextboxComponent } from './textbox/textbox.component';
+import { DatepickComponent } from './datepick/datepick.component';
 
 @NgModule({
   declarations: [
     NavigationComponent,
     TextboxComponent,
+    DatepickComponent,
   ],
   imports: [
     ReactiveFormsModule,
@@ -16,6 +18,7 @@ import { TextboxComponent } from './textbox/textbox.component';
   exports: [
     NavigationComponent,
     TextboxComponent,
+    DatepickComponent,
   ]
 })
 export class CoreModule { }
